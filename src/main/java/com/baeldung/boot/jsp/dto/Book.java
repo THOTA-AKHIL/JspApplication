d4d625f6d6ee93815ca7f0c5a30138e52eb98679
Akhil <thota.akhil@capgemini.com>
2022-12-09 19:11:12 +0530

    Initial commit

---
+++ src/main/java/com/baeldung/boot/jsp/dto/Book.java
@@ -0,0 +1,38 @@
+package com.baeldung.boot.jsp.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Book {
+    private String isbn;
+    private String name;
+    private String author;
+	public Book(String isbn2, String name2, String author2) {
+		// TODO Auto-generated constructor stub
+	}
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
---
+++ src/main/java/com/baeldung/boot/jsp/repository/model/BookData.java
@@ -0,0 +1,37 @@
+package com.baeldung.boot.jsp.repository.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookData {
+    public BookData(String string, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	private String isbn;
+    private String name;
+    private String author;
+}
